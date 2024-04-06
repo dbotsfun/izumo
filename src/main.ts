0fa15a3d829c378d135d5c3fa3713c5692bacdd5
simxnet <simxnet@envs.net>
2024-04-06 17:17:44 +0200

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	app.enableCors({
+		origin: [/^(.*)/],
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		preflightContinue: false,
+		credentials: true,
+		allowedHeaders:
+			'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for'
+	});
+
 	await app.listen(process.env.API_PORT || 3000);
 }
 
