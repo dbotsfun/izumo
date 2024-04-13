90e4b1d0fd586d0dfd0fa729f0351d9e3f676b23
Simxnet <95551108+simxnet@users.noreply.github.com>
2024-04-13 14:09:44 +0200

    feat(vote): check vote (#15)
    * feat: add `checkVote` query
    
    * fix: change query name to match its response
    
    * fix: logic
    
    * fix: oopsie
    
    Co-authored-by: Chiko <53100578+chikof@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Chiko <53100578+chikof@users.noreply.github.com>

--- src/modules/bot/resolvers/vote.resolver.ts
+++ src/modules/bot/resolvers/vote.resolver.ts
@@ -2,7 +2,7 @@ import { User } from '@modules/auth/decorators/user.decorator';
 import { JwtAuthGuard } from '@modules/auth/guards/jwt.guard';
 import type { JwtPayload } from '@modules/auth/interfaces/payload.interface';
 import { UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ValidationTypes } from 'class-validator';
 import { BotVoteCreateInput } from '../inputs/vote/create.input';
 import { BotVoteObject } from '../objects/vote/vote.object';
@@ -19,6 +19,22 @@ export class BotVoteResolver {
 	 * @param voteService - The vote service.
 	 */
 	public constructor(private readonly voteService: BotVoteService) {}
+
+	/**
+	 * Checks if an user is able to vote a bot.
+	 *
+	 * @param input - The input data for checking the vote.
+	 * @param user - The authenticated user making the vote.
+	 * @returns A Promise that resolves to the checked vote.
+	 */
+	@Query(() => Boolean)
+	@UseGuards(JwtAuthGuard)
+	public async canVote(
+		@Args('input') input: BotVoteCreateInput,
+		@User() user: JwtPayload
+	) {
+		return this.voteService.canVote(input.botId, user.id);
+	}
 
 	/**
 	 * Creates a vote for a bot.
