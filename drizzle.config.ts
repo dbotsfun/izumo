f6a453ac02f185722e9aa6758d4625971ba78906
Chiko <degnan200@gmail.com>
2024-04-27 21:07:21 +0100

    chore: drizzle-kit beta version

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,20 @@
 import 'dotenv/config'; // make sure to install dotenv package
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-export default {
-	driver: 'pg',
+export default defineConfig({
+	dialect: 'postgresql',
 	out: './src/database',
 	schema: './src/database/schema.ts',
 	dbCredentials: {
 		// biome-ignore lint/style/noNonNullAssertion: <explanation>
-		connectionString: process.env.DATABASE_URL!
+		url: process.env.DATABASE_URL!
 	},
 	// Print all statements
 	verbose: true,
 	// Always ask for confirmation
-	strict: true
-} satisfies Config;
+	strict: true,
+	migrations: {
+		table: 'drizzle_migrations',
+		schema: 'public'
+	}
+});
