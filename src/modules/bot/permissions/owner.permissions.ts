636be7e591b60e2ef94926117eb7a226385b5ef3
Chiko <degnan200@gmail.com>
2024-05-07 20:10:56 +0100

    docs: i love docs

--- src/modules/bot/permissions/owner.permissions.ts
+++ src/modules/bot/permissions/owner.permissions.ts
@@ -4,16 +4,16 @@ import { BitField } from '@sapphire/bitfield';
  * The flags for the bot owner permissions.
  */
 export const BotOwnerPermissionsFlag = {
-	SyncStats: 1 << 0,
-	SyncCommands: 1 << 1,
+	SyncStats: 1 << 0, // 1
+	SyncCommands: 1 << 1, // 2
 
-	ManageReview: 1 << 2,
-	ManageTags: 1 << 3,
-	ManageApiKey: 1 << 4,
-	ManageBot: 1 << 5,
-	ManageWebhook: 1 << 6,
+	ManageReview: 1 << 2, // 4
+	ManageTags: 1 << 3, // 8
+	ManageApiKey: 1 << 4, // 16
+	ManageBot: 1 << 5, // 32
+	ManageWebhook: 1 << 6, // 64
 
-	Admin: 1 << 7
+	Admin: 1 << 7 // 128
 };
 
 /**
