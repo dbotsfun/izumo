6e682e89a0c173b29cb610273ef0756e0c6084d3
Chikof <chiko@envs.net>
2024-06-09 00:45:25 +0100

    fix(throttler): because our api runs behind a reverse proxy server

--- src/lib/utils/guards/throttler.guard.ts
+++ src/lib/utils/guards/throttler.guard.ts
@@ -2,6 +2,7 @@ import { GQLExecutionContext } from '@lib/types';
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { ThrottlerGuard } from '@nestjs/throttler';
+import { Request } from 'express';
 
 @Injectable()
 export class GqlThrottlerGuard extends ThrottlerGuard {
@@ -10,5 +11,10 @@ export class GqlThrottlerGuard extends ThrottlerGuard {
 	): GQLExecutionContext {
 		const gqlCtx = GqlExecutionContext.create(context);
 		return gqlCtx.getContext<GQLExecutionContext>();
+	}
+
+	public override async getTracker(req: Request) {
+		// biome-ignore lint/style/noNonNullAssertion: Ibviously req.ip is defined
+		return req.ips ? req.ips[0] : req.ip!;
 	}
 }
